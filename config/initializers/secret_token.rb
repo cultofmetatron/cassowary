79136429618687ef65288db91fdda0bdac8245d3
Cultofmetatron <cultofmetatron@aumlogic.com>
2012-03-10 14:15:29 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cassowary::Application.config.secret_token = '011bc58e5015bbdb194f2bfd3d5c403cc403a281eec77009225c8c6d555382983afb88900b6d2106232dafa3b4b177913919d89c664b646d873b257cee42b64a'
