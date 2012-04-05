e3a40e0fdcce509da4ad4c518c9f02d2c45e3aa2
Cultofmetatron <cultofmetatron@aumlogic.com>
2012-04-05 19:48:10 -0400

    lightbox is done for now.  Onto getting markdown working

--- app/assets/javascripts/glambox.js
+++ app/assets/javascripts/glambox.js
@@ -7,18 +7,33 @@
 	var self = this;
 	var lightboxIn  = function(event) {
 			event.preventDefault();
+			
 			var img_path = $(this).data("fullsize");
-			var full_path = '<img src="' + img_path + '">';
-			$("div#photo_gallery_panel > div").css({
-				"background-image":"url(" + img_path + ")",
-			});
+			var winheight = $(window).height();
+			var winwidth = $(window).width();
+			var picheight = winheight*.7;
+			var picwidth = winwidth*.7
+			
+			//calculate offsetts
+			var offset_vertical = (winheight - picheight) / 2 - 40 ;
+			//alert(offset_vertical);
+			
+			var img_html;
+			img_html = '<img src="' + img_path + '" ' ;
+			img_html = img_html + 'width=' + picwidth + ' height="' + picheight + '" ';
+			img_html = img_html + '>';
+			
+			$("div#photo_gallery_panel > div").html(img_html);
+			$("div#photo_gallery_panel > div > img").css("margin-top", offset_vertical )
+			
+			
 			$("div#photo_gallery_panel").fadeIn("slow");
 	}
 	$('ul > li', self).delegate( 'a', "click", lightboxIn);
 	
 	$("div#photo_gallery_panel").bind({
 		"click": function(event) {
-			$(this).fadeOut("slow");
+			$(this).fadeOut();
 		},
 	})
 	
@@ -26,5 +41,6 @@
 })( jQuery );
 
 $(document).ready(function() {
+	
 	$('div#photo_gallery').glamBox();
 })
